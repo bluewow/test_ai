4c7770d3a260a8c5d525bdeb671228616674955b
bluewow <dogseen@gmail.com>
2024-01-02 19:49:19 +0900

    html parsing

--- home.py
+++ home.py
@@ -12,7 +12,7 @@ expected = sidebar.text_area('기대값')
 # 상단의 단일 텍스트 박스 추가
 step1 = st.text_area("step1 로그인", "", height=100)
 step2 = st.text_area("step2 테스트 케이스 실행", "", height=100)
-step3 = st.text_area("step3", "기대값 매칭", height=100)
+step3 = st.text_area("step3 기대값 매칭", "", height=100)
 
 # 사이드바에 버튼 추가
 if sidebar.button('버튼 클릭'):
--- retrieval.py
+++ retrieval.py
@@ -7,27 +7,22 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import BSHTMLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
+
 
 def rag(url, driver):
     driver.get(url) 
     time.sleep(1)
     html = driver.page_source
 
-    # HTML 내용을 로컬 파일로 저장
-    with open('temp.html', 'w', encoding='utf-8') as file:
-        file.write(html)
-
     # RAG
-    loader = UnstructuredHTMLLoader("temp.html")
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=600,
-        chunk_overlap=100,
+    html_splitter = RecursiveCharacterTextSplitter.from_language(
+        language=Language.HTML, chunk_size=60, chunk_overlap=0
     )
-    docs = loader.load_and_split(text_splitter=splitter)
-    vectorstore = FAISS.from_documents(docs, OpenAIEmbeddings())
+    html_docs = html_splitter.create_documents([html])
+    vectorstore = FAISS.from_documents(html_docs, OpenAIEmbeddings())
     retriever = vectorstore.as_retriever()
     
-    query = "find login inputbox element id"
-    result = retriever.get_relevant_documents(query)
-    
-    print(result)
+    print(retriever.get_relevant_documents("find 아이디 input box class id"))
+    print("------------------")
+    print(retriever.get_relevant_documents("find 비밀번호 input box class id"))
