2e4f323a984195b49a508decdd4078c6a111d4b1
bluewow <dogseen@gmail.com>
2024-01-05 08:02:16 +0900

    bridegemodel 추가

--- bridgeModel.py
+++ bridgeModel.py
@@ -1,3 +1,24 @@
+actions_list = ["로그인", "로그아웃", "클릭", "선택", "체크"]
 
 def conectCaseAndModel(case):
-    print(case)
+    if case.target is not None:
+        print(case.target)
+    
+        # action list와 case.action이 일치하면 실행
+    if case.action in actions_list:
+        if case.action == "로그인":
+            # 로그인에 대한 동작 수행
+            pass
+        elif case.action == "로그아웃":
+            # 로그아웃에 대한 동작 수행
+            pass
+        elif case.action == "클릭":
+            # 클릭에 대한 동작 수행
+            pass
+        elif case.action == "선택":
+            # 선택에 대한 동작 수행
+            pass
+        elif case.action == "체크":
+            # 체크에 대한 동작 수행
+            pass
+    
